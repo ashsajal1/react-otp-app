6395a86519c627ac3f75f700a40254f1435ea18f
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-04-16 19:11:21 +0600

    design container layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import { useState } from "react";
-import OtpPrompt from "./components/otp-prompt";
-import { toast, Toaster } from 'react-hot-toast'
+import OtpContainer from './components/otp-container'
 
-import './App.css'
 export default function App() {
-  const createOtp = () => {
-    const otp = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
-    return otp;
-  }
-
-  const [otp, setOtp] = useState<number | null>(null)
-  const [isSent, setIsSent] = useState(false);
-
-  const handleSendOtp = () => {
-    const updatedOtp = createOtp();
-    setOtp(updatedOtp)
-    setIsSent(true)
-    toast.success(`Your otp is ${updatedOtp}!`)
-  }
-
   return (
-    <div className="wrapper">
-      <Toaster />
-      <div>
-        <OtpPrompt />
-        <button className="submit-btn">Submit</button>
-      </div>
-      <button onClick={handleSendOtp}>{isSent ? 'Resend' : 'Send'} otp</button>
-    </div>
+    <OtpContainer />
   )
 }
---
+++ src/components/otp-container.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { toast, Toaster } from 'react-hot-toast'
+
+import OtpPrompt from "./otp-prompt";
+import { createOtp } from "../lib/createOtp";
+import './otp-container.css'
+export default function OtpContainer() {
+    const [otp, setOtp] = useState<number | null>(null)
+    const [isSent, setIsSent] = useState(false);
+
+    const handleSendOtp = () => {
+        const updatedOtp = createOtp();
+        setOtp(updatedOtp)
+        setIsSent(true)
+        toast.success(`Your otp is ${updatedOtp}!`)
+    }
+
+    return (
+        <div className="container">
+            <Toaster />
+            <div>
+                <OtpPrompt />
+                <button className="submit-btn">Submit</button>
+            </div>
+            <button onClick={handleSendOtp}>{isSent ? 'Resend' : 'Send'} otp</button>
+        </div>
+    )
+}
