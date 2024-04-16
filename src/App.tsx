6bcc316d963bca19de749a3c40d940c37d313968
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-04-16 17:44:04 +0600

    create otp function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 export default function App() {
+  const createOtp = () => {
+    const otp = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+    return otp;
+  }
+
   return (
-    <div>App</div>
+    <div>
+      {createOtp()}
+    </div>
   )
 }
