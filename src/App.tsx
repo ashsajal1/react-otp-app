18384c8881c73ca32b4e1cc14e6e0bf7f1437834
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-04-16 17:53:23 +0600

    add btn to send resend otp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 export default function App() {
   const createOtp = () => {
     const otp = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
     return otp;
   }
 
+  const [otp, setOtp] = useState<number | null>(null)
+  const [isSent, setIsSent] = useState(false);
+
+  const handleSendOtp = () => {
+    const updatedOtp = createOtp();
+    setOtp(updatedOtp)
+    setIsSent(true)
+  }
+
   return (
     <div>
-      {createOtp()}
+      {otp}
+      <div>
+        <input title="otp box" type="number" />
+        <input title="otp box" type="number" />
+        <input title="otp box" type="number" />
+        <input title="otp box" type="number" />
+      </div>
+      <button onClick={handleSendOtp}>{isSent ? 'Resend':'Send'} otp</button>
     </div>
   )
 }
